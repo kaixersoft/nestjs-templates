ddb385ece57067da55a03e49c0085cf604a39845
xerxis-emapta <xerxis.alvar@emapta.com>
2024-03-01 12:07:53 +0800

    removed missing module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { BullboardModule } from './bullboard/bullboard.module';
 import { SampleModule } from './sample/sample.module';
-import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [BullboardModule, SampleModule, ProfileModule],
+  imports: [BullboardModule, SampleModule],
   controllers: [AppController],
 })
 export class AppModule {}
